fdcac8c9ad1e93f1ea6b144b65cb3ac349f02c29
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-08-16 15:46:33 +0700

    Fixes #1, Fixes #2

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -1,13 +1,17 @@
 package tictactoe;
 
 import java.util.function.Predicate;
-
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.Property;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Node;
 
-
+/**
+ * This class is for initializing the board and the function of
+ * the game. For example, initializing the board and the X,O pieces.
+ *
+ * @author Thanapoom Rattanathumawat
+ *
+ */
 public class TicTacToeGame {
 	private final int boardsize;
 	/** View of the TicTacToe board. */
@@ -33,8 +37,8 @@ public class TicTacToeGame {
 	
 	public void startNewGame() {
 		// Avoid nulls. Assign a "none" object to each location on the board.
-		for(int row=0; row<3; row++) 
-			for(int col=0; col<3; col++) pieces[row][col] = Piece.NONE;
+		for(int row=0; row<boardsize; row++)
+			for(int col=0; col<boardsize; col++) pieces[row][col] = Piece.NONE;
 		// Remove Pieces from the board (view), but not the squares themselves. Use a Predicate to test for Piece.
 		Predicate<Node> isPiece = (node) -> node instanceof Piece;
 		board.getChildren().removeIf(isPiece);
